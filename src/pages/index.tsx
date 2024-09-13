f31b1bf34b2cdb59981a7af0831a778608998d1f
vedze <ezdev00@gmail.com>
2024-09-13 21:27:16 +0900

    페이지 별로 다른 layout 적용시키기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
+import SearchableLayout from "@/components/searchable-layout";
+import { ReactNode } from "react";
+
 export default function Home() {
   return <h1>인덱스</h1>;
 }
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
